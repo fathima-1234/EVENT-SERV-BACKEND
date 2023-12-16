c773d1beb787a548851dae06a21cb330043f828f
fathima-1234 <fathimasuhra19@gmail.com>
2023-12-16 14:56:41 +0530

    hosting

--- backend/settings.py
+++ backend/settings.py
@@ -180,6 +180,8 @@ DATABASES = {
         "PORT": config('DB_PORT', default='', cast=int),
     }
 }
+import dj_database_url
+DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 MEDIA_URL = '/photos/'
