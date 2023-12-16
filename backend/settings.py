3e7b04eb123c03c1e4cdeffcc2233cf310cfa8b6
fathima-1234 <fathimasuhra19@gmail.com>
2023-12-16 14:59:51 +0530

    hostingnext

--- backend/settings.py
+++ backend/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
