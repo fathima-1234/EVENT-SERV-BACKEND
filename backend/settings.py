eeb9dfedbc67fe7ed74e7190306cd13bb9ba9f59
fathima-1234 <fathimasuhra19@gmail.com>
2023-12-18 13:56:58 +0530

    add allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -195,7 +195,7 @@ DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -232,7 +232,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = "base.User"
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000",
 
-                      "http://127.0.0.1:8000",
+                      "https://event-serv.vercel.app/",
                       ]
 
 CORS_ALLOW_METHODS = [
